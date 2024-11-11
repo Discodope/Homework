14a19d5309d661c18fef5bd82c0e8ef7d429cc73
Даниил <daniil.lebedinsky@gmail.com>
2024-11-11 21:19:49 +0300

    1

---
+++ HomeLesson.py
@@ -0,0 +1,13 @@
+my_string = input('На кого ты обучаешься ? ') # Ввод -  На программиста #
+len(my_string)
+print(len(my_string))
+
+print(my_string .upper())
+
+print(my_string .lower())
+
+print('На программиста'.replace(' ',''))
+
+print('На программиста'[0])
+
+print('На программиста' [-1])
